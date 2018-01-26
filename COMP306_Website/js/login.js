bc4325e37a79b0d42eef8fad9d7a0c45693289b0
alessioRM87 <alessio.iannella@gmail.com>
2018-01-26 15:04:23 -0500

    // COMPLETED PROJECT
    Imported final Website project into new repository

---
+++ COMP306_Website/js/login.js
@@ -0,0 +1,84 @@
+﻿window.onload = function () {
+
+    $("buttonLogin").keyup(function () {
+
+        var username = $("username").val();
+        var password = $("password").val();
+
+        $.post(
+            "http://localhost:17854/api/users/login",
+            {
+                username: username,
+                password: password
+            },
+            function (result) {
+                console.log(result);
+            }
+        );
+
+
+
+    };
+}
+
+    //getCookie();
+
+    //function getCookie(username) {
+    //    var name = cname + "=";
+    //    var ca = document.cookie.split(';');
+    //    for (var i = 0; i < ca.length; i++) {
+    //        var c = ca[i];
+    //        while (c.charAt(0) == ' ') {
+    //            c = c.substring(1);
+    //        }
+    //        if (c.indexOf(name) == 0) {
+    //            return c.substring(name.length, c.length);
+    //        }
+    //    }
+    //    return "";
+    //}
+
+    //function checkCookie() {
+    //    var user = getCookie("username");
+    //    if (user != "") {
+    //        alert("Welcome again " + user);
+    //    } else {
+    //        user = prompt("Please enter your name:", "");
+    //        if (user != "" && user != null) {
+    //            setCookie("username", user, 365);
+    //        }
+    //    }
+    //}
+
+
+};
+
+function login() {
+
+    $.post()
+
+    var xhr = new XMLHttpRequest();
+    var url = "http://localhost:17854/api/users/login";
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json");
+
+    
+
+    var json = {
+        username: username,
+        password: password
+    };
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            console.log("SUCCESS");
+        }
+    };
+
+    xhr.onerror = function () {
+        console.log(xhr);
+    }
+
+    xhr.send(json);
+}
+
---
+++ COMP306_Website/js/upload.js
@@ -0,0 +1,60 @@
+﻿function upload() {
+
+    var postBookRequest = new XMLHttpRequest();
+    var url = "http://localhost:17854/api/books";
+    postBookRequest.open("POST", url, true);
+    postBookRequest.setRequestHeader("Content-type", "application/json");
+
+    var isbn = document.getElementById('isbn').value;
+    var title = document.getElementById('title').value;
+    var author = document.getElementById('author').value;
+    var date = document.getElementById('date').value;
+
+    var json = {
+        isbn: isbn,
+        title: title,
+        author: author,
+        imageURL: "",
+        downloadURL: "",
+        publishedDate: date
+    };
+
+    postBookRequest.onreadystatechange = function () {
+        if (postBookRequest.readyState === 4 && postBookRequest.status === 200) {
+            var postBookJSON = JSON.parse(postBookRequest.responseText);
+            console.log("SUCCESS");
+            console.log(postBookJSON);
+
+            
+
+            var postImageRequest = new XMLHttpRequest();
+            postImageRequest.open('POST', url, true);
+
+            postImageRequest.onreadystatechange = function () {
+                if (postImageRequest.readyState === 4 && postImageRequest.status === 200) {
+                    var postImageJSON = JSON.parse(postImageRequest.responseText);
+                    console.log("SUCCESS");
+                    console.log(postImageJSON);
+                }
+
+            };
+            postImageRequest.onerror = function (error) {
+                console.log(error);
+            };
+            postImageRequest.send(formData);
+        }
+        else if (postBookRequest.readyState === 4 && postBookRequest.status === 400) {
+            postBookJSON = JSON.parse(postBookRequest.responseText);
+            console.log("FAILURE");
+            console.log(postBookJSON);
+        }
+        else if (postBookRequest.readyState === 4 && postBookRequest.status === 404) {
+            postBookJSON = JSON.parse(postBookRequest.responseText);
+            console.log("FAILURE");
+            console.log(postBookJSON);
+        }
+    };
+
+    postBookRequest.send(json);
+    
+}
